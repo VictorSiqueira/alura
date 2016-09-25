83c63747cf4b3b1104b7d51ac4f7274967837738
Victor Siqueira <victorsiqueiraj@gmail.com>
2016-09-24 22:16:59 -0300

    conclusao da primeira aula

--- Nodejs/projetos/casadocodigo/app.js
+++ Nodejs/projetos/casadocodigo/app.js
@@ -1,8 +1,16 @@
+//usando a lib nodemon, o server será startado com ele e pegara qualquer alteração que for realizada no projeto
 var express = require('express');
 var server = express();
 
+/*satando o ejs no servidor, nao achei muito bacana, 
+pois aparentemento substitui todos os aruivos html para a extensao ejs. fica parecendo jsp do java
+o que faz a aplicação ficar amarrada e nao modular*/
+server.set('view engine', 'ejs');
+
+
 server.get('/produtos', function(req, res){
-	res.send('<html><body>Listagem de produtos</body></html>')
+	//res.send('<html><body>Listagem de produtos</body></html>');
+	res.render("index");//chamada do arquivo
 })
 
 server.listen(3000, function(){

b20b520e24474422e7b14d2c299a7ef1e748135d
VictorSiqueira <victorsiqueiraj@gmail.com>
2016-10-04 22:03:53 -0300

    primeira parte da aula 7

--- Nodejs/projetos/casadocodigo/app/infra/dbConnectionFactory.js
+++ Nodejs/projetos/casadocodigo/app/infra/dbConnectionFactory.js
@@ -2,13 +2,20 @@
 var mysql = require('mysql');
 
 function createDBConnection(){
-	console.log('conexao com DB');
-	return  mysql.createConnection({
-				host : "localhost",
-				user: "root",
-				password: "",
-				database : "nodejs"
-			});
+/*o epxress tem uma varival chamada 'NODE_ENV', dentrod objeto process.env, que conseuge verificar se voce esta num ambiante de desenv ou producao,
+pois pra producao ele faz algumas otimizações como deixar as views em cache para otimizar o carregamento*/
+	//fazendo verificação de ambiente de desenvolvimento
+	if(!process.env.NODE_ENV){
+		console.log('conexao com DB');
+		return  mysql.createConnection({
+					host : "localhost",
+					user: "root",
+					password: "",
+					database : "nodejs"
+				});
+	}else if(process.env.NODE_ENV == 'test'){
+
+	}
 }
 
 //wrapper
---
+++ Nodejs/projetos/casadocodigo/test/produtos.js
@@ -0,0 +1,63 @@
+//usando o mocha para realizar teste com o nodejs
+//ele sempre busca a pasta 'test'
+
+/*Teste manual usando mocha
+var http = require('http');
+var assert = require('assert');//lib para testes e respostas automaizadas, parecido com junit do java
+
+
+
+//monstagem do teste, esses primeiros paramettros no describw e no it são apenas alias
+//colocar '#' serve apenas para facilitar no verbosa do console
+describe('#ProdutosController', function(){//cenario
+	//quando coloco uma função de parametro no it, ela funciona para fazer o it entender que tem chamadas assincronas e ele precisa esperar
+	it('#listagem json', function(done){
+
+		var config = {
+			hostname: 'localhost',
+			port: 3000,
+			path: '/produtos',
+			headers: {
+				'Accept': 'application/json'
+			}
+		}
+		
+		http.get(config, function(res){
+			//primeiro valor variavel, segundo é o valor esperado para ele dizer se é true
+			assert.equal(res.statusCode, 200);
+			assert.equal(res.headers['content-type'],'application/json');
+			done();//chaamo a função apos realizar os testes, dentro do callback da chamada assincrona
+		})
+	});
+});*/
+
+
+/*Teste usando a lib supertest, que serve para testes de integração, verificando headers e etc*/
+//saindo da pasta chamando o arquivo express criado por mim, que sobe o express configurado
+var express = require('../config/expressConfig')();
+/*passando o express para o construtor do supertest que espera uma estancia de server,
+isso faz com que nao seja mais necessario subir as instacias de server para realizar osteste
+pois ele ja estara fazendo issso para fazer as requisições*/
+var request = require('supertest')(express);
+describe('#ProdutosController', function(){
+	it('#listagem json', function(done){		
+		request.get('/produtos')//passando a rota que deseja testar
+			//objeto retornado para configuraçções relativas a chamada
+			.set('Accept','application/json') //fazendo verificação de headers
+			.expect('Content-type', /json/)
+			.expect(200, done);//passando numero no primeiro parametro indica que é o atributo de status, e o done serve para indicar o encerramento
+	});
+
+	it('#cadastro de produto invalido', function(done){
+		request.post('/produtos/salva')
+			.send({titulo: '', descricao:'novo livro'})//passa o argumentos para a requisição
+			.expect(400, done);
+	});
+
+	it('#cadastro de produto valido', function(done){
+		request.post('/produtos/salva')
+			.send({titulo: 'teste', descricao:'novo livro', preco: 1.50})//passa o argumentos para a requisição
+			.expect(302, done);
+	});
+});
+
